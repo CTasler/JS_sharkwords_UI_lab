b56c86cd14965c093bc917503fe4ef723c5a1226
Claire Tasler <tasl0368@gmail.com>
2022-10-18 16:28:28 -0500

    finished disableLetterButton and isLetterInWord functions
    Co-author: Lily Yang

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -1,3 +1,5 @@
+
+
 const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
 
 const WORDS = [
@@ -17,10 +19,9 @@ const WORDS = [
 
 const numWrong = 0;
 
-// Loop over the chars in `word` and create divs.
-// The divs should be appended to the section with id="word-container".
+
 const createDivsForChars = (word) => {
-  // Replace this with your code
+
   const container = document.querySelector("#word-container");
   for (const letter of word){
     container.insertAdjacentHTML('beforeend', 
@@ -31,29 +32,32 @@ const createDivsForChars = (word) => {
 // Loop over each letter in the alphabet and generate a button for each letter
 // The buttons should be appended to the section with id="letter-buttons"
 const generateLetterButtons = () => {
-  // Replace this with your code
+
   const container = document.querySelector("#letter-buttons");
   for (letter of ALPHABET){
     container.insertAdjacentHTML('beforeend', `<button>${letter}</button>`);
   }
 };
 
-// Set the `disabled` property of `buttonEl` to `true.
-//
-// `buttonEl` is an `HTMLElement` object.
-//
+
 const disableLetterButton = (buttonEl) => {
-  // Replace this with your code
-  const container = document.querySelector("#letter-buttons");
-  
+   const button = buttonEl;
+   console.log(button);
+   button.disabled = true;
 };
 
-// This is a helper function we will use in the future
-// It should return `true` if `letter` is in the word
-// For now, you should test it out to make sure it works
+
 
 const isLetterInWord = (letter) => {
-  // Replace this with your code
+  const letterDiv = document.querySelector(`.letter-box.${letter}`);
+  if (letterDiv === null){
+      console.log(false)
+      return false
+  } 
+  else {
+    console.log(true)
+    return true;
+  }
 };
 
 // This is like if __name__ == '__main__' in Python
@@ -64,14 +68,15 @@ const isLetterInWord = (letter) => {
   // You can change this to choose a random word from WORDS once you
   // finish this lab but we hard code it so we know what the word is
   // and can tell if things look correct for this word
-  const word = 'hello';
+  const word = WORDS[Math.floor((Math.random() * WORDS.length))];
+  console.log(word)
   createDivsForChars(word);
   // call the function that makes an empty line for each letter in the word
   // Replace this line with the function call
   generateLetterButtons();
   // call the function that makes a button for each letter in the alphabet
   // Replace this line with the function call
-
+  
   // in the next lab, you will be adding functionality to handle when
   // someone clicks on a letter
 })();
